88b13316789101f289e6697cfb1eab0cccb02e35
Esra Al <esra.wameed@yahoo.com>
2022-11-15 19:58:33 -0500

    added an image of me to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/header.css';
-
+import Me from '../images/me.png';
 function Header() {
     return (
       <header className="header">
         <h1>Esra Al-Abduljabar</h1>
+        <img src={Me} alt="me smiling."></img>
       </header>
     );
   }
