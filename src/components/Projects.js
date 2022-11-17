3d7faf8db257262ec2c83372d8fa8c5a47405ba7
Esra Al <esra.wameed@yahoo.com>
2022-11-17 17:12:34 -0500

    ensure to add styling to project tab

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,24 @@
 import React from "react";
-
-const styles = {
-  img: {
-    maxWidth: "750px",
-  },
-};
+import '../styles/portfolio.css';
 
 export default function Portfolio(props) {
   return (
     <div>
       {props.projects.map((project) => (
-        <div className="card" key={project.id}>
+        <div className="" key={project.id}>
           <img
             src={project.projectImg}
-            className="card-img-top"
+            className=""
             alt="..."
-            style={styles.projectImg}
           />
-          <div className="card-body">
-            <h5 className="card-title">{project.title}</h5>
-            <p className="card-text desc">{project.projectSummary}</p>
-            <a href={project.repo} className="btn btn-primary">
+          <div className="">
+            <h5 className="">{project.title}</h5>
+            <p className="">{project.projectSummary}</p>
+            <a href={project.repo} className="">
               View Github
             </a>
             {project.deployDemo ? (
-              <a href={project.deployDemo} className="btn btn-primary">
+              <a href={project.deployDemo} className="">
                 View Site
               </a>
             ) : null}
