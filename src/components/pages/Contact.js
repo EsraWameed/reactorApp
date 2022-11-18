9da130f10f156352e904f80cd6db96d2256c0b91
Esra Al <esra.wameed@yahoo.com>
2022-11-17 23:19:59 -0500

    added email and phone number

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,79 +3,73 @@ import { validateEmail } from "../../utils/helper.js";
 import '../../styles/contact.css';
 function Contact() {
 
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [err, setErr] = useState("");
-  const [message, setMessage] = useState('');
-
-  const handleInputChange = (event) => {
-    const {target} = event;
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleInputChange = (e) => {
+    const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-
     if (inputType === "email") {
-      setEmail(inputValue);
-    } else {
-      setName(inputValue);
+      if (!validateEmail(inputValue)) {
+        setErrorMessage("Invalid email");
+        return;
+      } else {
+        setErrorMessage("");
+        return;
+      }
+    } else if (inputType === "message") {
+      if (!inputValue) {
+        setErrorMessage("Message is required");
+        return;
+      } else {
+        setErrorMessage("");
+        return;
+      }
     }
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    if (!validateEmail(email) || !name) {
-      setErr("Please enter a valid email");
-
-      return;
-    }
-    setName("");
-    setEmail("");
-    setMessage("")
-    setErr("")
   };
 
   return (
+    <div>
+      <h1>To get in touch with me, please reach me at abduljabaresra0@gmail.com</h1>
+      <h2>Phone: 6043387943</h2>
+        {errorMessage && (
+        <div className="errorHandle">
+          <p>{errorMessage}</p>
+        </div>
+      )}
     <div className="formContainer">
       <form class="formTwo">
         <input
         className="form-control"
-          value={name}
           name="name"
           onChange={handleInputChange}
-          onMouseOut={handleInputChange}
           type="text"
-          placeholder="Enter your name"
+          placeholder="name"
          
         />
         <input
           className="form-control"
-          value={email}
           name="email"
           onChange={handleInputChange}
-          onMouseOut={handleInputChange}
           type="email"
-          placeholder="Enter your email"
+          placeholder="email"
+          required
           
         />
 <textarea
             className="form-control"
-          value={message}
           name="message"
           onChange={handleInputChange}
-          onMouseOut={handleInputChange}
           type="text"
           placeholder="Message"
         />
-        <button type="button" className="btn btn-primary" onClick={handleFormSubmit}>
+        <button type="button" className="btn btn-primary">
           Submit
         </button>
       </form>
-      {err && (
-        <div>
-          <p>{err}</p>
-        </div>
-      )}
+     
     </div>
+    </div>
+    
   );
 }
 export default Contact;
