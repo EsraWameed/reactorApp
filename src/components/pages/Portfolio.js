5b6656afc003f02de2cde8142025f11a699a4b2c
Esra Al <esra.wameed@yahoo.com>
2022-11-29 13:10:01 -0500

    added project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,18 +6,27 @@ import Weather from "../../images/weather.png";
 import Quiz from "../../images/quiz.png";
 import Ezee from "../../images/Ezeecommerce.png";
 import Team from "../../images/teamGenerator.png";
+import Apollodae from "../../images/desktopView.jpg"
 
 const projects = [
   {
     id: 1,
-    title: "TEAGRAM",
-    projectSummary: "A RESTful API social media application allowing users to create profiles and interact with other users posts through liking, commenting, and downloading images.",
-    projectImg: Tea,
-    repo: "https://github.com/EsraWameed/teagram/",
-    deployDemo: "https://tea-gram.herokuapp.com/",
+    title: "ApolloDAE",
+    projectSummary: "Collaborated with a team to create a full stack E-commerce application. This application utilizes React and Tailwind for the front end, and MongoDB, node.js, and GraphQL for the back end. Users can log in, search for items by search bar or category, add/remove from the cart, and checkout. Admin can perform all the CRUD operations on products.",
+    projectImg: Apollodae,
+    repo: "https://github.com/abedhasan79/ApolloDAE",
+    deployDemo: "https://apollodae001.herokuapp.com/",
   },
   {
-    id: 2,
+  id: 2,
+  title: "TEAGRAM",
+  projectSummary: "A RESTful API social media application allowing users to create profiles and interact with other users posts through liking, commenting, and downloading images.",
+  projectImg: Tea,
+  repo: "https://github.com/EsraWameed/teagram/",
+  deployDemo: "https://tea-gram.herokuapp.com/",
+},
+  {
+    id: 3,
     title: "MOTIVATION LOCALE",
     projectSummary: "An application utilizing two server-side API’s to randomly generate motivational quotes, positive images, and store user feelings to lift user spirit.",
     projectImg: Motivation,
@@ -25,7 +34,7 @@ const projects = [
     deployDemo: "https://esrawameed.github.io/Motivation-Locale/",
   },
   {
-    id: 3,
+    id: 4,
     title: "WEATHER BOARD",
     projectSummary:
       "An application utilizing third-party APIs to retrieve daily weather data. Upon a user searching for a city, weather data, and a 5-day forecast are saved into localStorage.",
@@ -34,7 +43,7 @@ const projects = [
     deployDemo: "https://esrawameed.github.io/WeatherBoard/",
   },
   {
-    id: 4,
+    id: 5,
     title: "CODING QUIZ",
     projectSummary: "An application utilizing Web APIs, DOM methods to present users with a timed coding quiz that keeps track of their scores and reduces time on the timer when questions are answered incorrectly.",
     projectImg: Quiz,
@@ -42,7 +51,7 @@ const projects = [
     deployDemo: "https://esrawameed.github.io/Coding-Knowledge-Quiz-Game/",
   },
   {
-    id: 5,
+    id: 6,
     title: "E-COMMERCE",
     projectSummary:
       "This E-commerce backend node.js application utilizes express.js server and an SQL database. This RESTful APIs executes CRUD operations. This application uses sequelize to convert data to and from the database. Insomnia was used to demonstrate functionality when executing requests.",
@@ -51,7 +60,7 @@ const projects = [
     deployDemo: "https://www.youtube.com/watch?v=9ZIUlCNxBqo&ab_channel=EsraAl-Abduljabar",
   },
   {
-    id: 6,
+    id: 7,
     title: "PROFILEATOR",
     projectSummary: "This application was created to allow Managers of companies to efficiently create new roles/positions within their company, and add universal information about each member, such as name,id, and email. Alongside the universal information inputted for each member type, the different member types have their own customizable information inputs such as gitHub username for the Engineer, office number for the Manager, and school being attended by the intern.",
     projectImg: Team,
